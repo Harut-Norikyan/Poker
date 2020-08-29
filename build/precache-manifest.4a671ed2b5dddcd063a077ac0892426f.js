a80e6f27bd1abf28d69332cfe6dea9fb72ab7769
Harut <harutnoro@mail.ru>
2020-08-29 19:37:01 +0400

    build

---
+++ build/precache-manifest.4a671ed2b5dddcd063a077ac0892426f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62864e396edd74f4cc923a58575e07aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1d1a95ddc487c29c7b8",
+    "url": "/static/css/main.0ea46f55.chunk.css"
+  },
+  {
+    "revision": "c9fa30ad01847bbfad0b",
+    "url": "/static/js/2.ff2d8306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff2d8306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d1a95ddc487c29c7b8",
+    "url": "/static/js/main.b3351e1a.chunk.js"
+  },
+  {
+    "revision": "cfa89be4b7f8a9dfa4a8",
+    "url": "/static/js/runtime-main.b81be246.js"
+  },
+  {
+    "revision": "da8f6c789c2f112a6fc722b46c3671ab",
+    "url": "/static/media/club.da8f6c78.png"
+  },
+  {
+    "revision": "5853139a45543fb6a593cf068cf79f95",
+    "url": "/static/media/diamond.5853139a.png"
+  },
+  {
+    "revision": "524dd4ffecb471590ab0c5f76efddc5b",
+    "url": "/static/media/heart.524dd4ff.png"
+  },
+  {
+    "revision": "ccbe5c441b49a67cf9b6ec4af7cf41eb",
+    "url": "/static/media/table.ccbe5c44.png"
+  }
+]);
